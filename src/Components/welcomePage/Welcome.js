07f00caabb9f68cc3d693cce6a46632f71dff43d
unknown <dineshsoni8569@gmail.com>
2021-04-14 17:46:03 +0530

    welcome page

--- src/Components/welcomePage/Welcome.js
+++ src/Components/welcomePage/Welcome.js
@@ -1,17 +1,17 @@
 import { React } from "react";
 
 import "./Welcome.css";
-import SignIn from '../signin/SignIn'
+import SignIn from '../signin/SignIn';
+
 
 function Welcome() {
   return (
-    <div className="signIn">
-      <div className="signIn_intro">
-        <h1>HangOut</h1>
-        <h3>Welcome to Chat app</h3>
-      <SignIn />
+    <div className="welcome">
+      <div className="welcome_intro">
+        <h1>Gossip Point</h1>
       </div>
-      {/* <button className="signIn_btn"></button> */}
+      <div class="signin_btn">Enter</div>
+     
     </div>
   );
 }
